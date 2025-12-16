228423bb706ec759aa45a74aeda645b5741959ee
ydking0911 <ydking0911@gmail.com>
2025-12-16 22:51:43 +0900

    fix: CI 테스트 코드 수정

--- src/test/java/com/gpyeong/core/CoreApplicationTests.java
+++ src/test/java/com/gpyeong/core/CoreApplicationTests.java
@@ -3,6 +3,9 @@ package com.gpyeong.core;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
 @SpringBootTest
 class CoreApplicationTests {
 
